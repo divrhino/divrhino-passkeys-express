6e0bd503ca371473638fb7850662256a29929347
Div Rhino Dev <65655808+divrhino@users.noreply.github.com>
2023-12-28 21:45:48 +0800

    Step 3 - Postgres setup (#3)
    * Use .env variables in docker-compose
    * Add `db` service to docker-compose
    * Install postgres package
    * Add database instance helper
    * Add first model - `User`

---
+++ db/migrations/20231228134212-create-user.js
@@ -0,0 +1,32 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            email: {
+                type: Sequelize.STRING,
+                unique: true,
+            },
+            handle: {
+                type: Sequelize.BLOB,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('users')
+    },
+}
