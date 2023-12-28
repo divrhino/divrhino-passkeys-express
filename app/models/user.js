ce28ffcef25a826ef756ba2d5144c91157627314
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 22:10:03 +0800

    Add association in user for public key credentials

--- app/models/user.js
+++ app/models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            this.hasMany(models.PublicKeyCredentials, { foreignKey: "user_id" })
         }
     }
     User.init(
