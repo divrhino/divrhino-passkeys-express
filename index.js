d902b630220d267030e38c147be0a5b9c8ef1cd6
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 21:53:59 +0800

    Configure app to use ejs and layouts

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express()
 const port = process.env.PORT || 3000
 const host = '0.0.0.0'
 
+const path = require('path')
+const layouts = require('express-ejs-layouts')
+
+// Templates
+app.use(layouts)
+app.set('views', path.join(__dirname, 'app/views'))
+app.set('layout', 'layouts/application')
+app.set('view engine', 'ejs')
+
 // Routes
 app.use('/', require('./config/routes'))
 
