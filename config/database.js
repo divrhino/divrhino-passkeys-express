d3185305d8b3c6d5cee0ea9de2b8358e8dbb1dae
Div Rhino Dev <65655808+divrhino@users.noreply.github.com>
2023-12-28 21:35:00 +0800

    Step 2 - Configure Sequelize (#2)
    * Add formatting
    * Install sequelize package
    * Customise sequelize config file placement

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const dbSettings = {
+    development: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        dialect: 'postgresql'
+    },
+    test: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        dialect: 'postgresql'
+    },
+    production: {
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        host: process.env.PGHOST,
+        dialect: 'postgresql'
+    }
+}
+
+module.exports = dbSettings
