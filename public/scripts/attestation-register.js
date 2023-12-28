4bdd8ed2f435b8c6a81a5a2a47b885a9267bd56c
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 22:42:06 +0800

    Phase 1 - step 1 - getChallenge

--- public/scripts/attestation-register.js
+++ public/scripts/attestation-register.js
@@ -1,9 +1,22 @@
 class Register {
     async init(event) {
         // 1. Get Challenge from server (Relying Party)
+        const challenge = await this.getChallenge(event)
         // 2. Use challenge to create public key credential pair
         // 3. Send publicKey+challenge to server to create new user
         // 4. Redirect to user's dashboard
+    }
+
+    async getChallenge(event) {
+        const response = await fetch('/register/public-key/challenge', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+            }, 
+            body: new FormData(event.target),
+        })
+
+        return response.json()
     }
     
 }
