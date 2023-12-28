33cdb466c77b7b9cf00d3220e45c00e640f971bf
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 22:28:02 +0800

    Add necessary config to index.js for sessions

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const db = require('./db/helpers/init')
+
 const app = express()
 const port = process.env.PORT || 3000
 const host = '0.0.0.0'
 
 const path = require('path')
 const layouts = require('express-ejs-layouts')
+
+// Session store
+const sessionStore = new SequelizeStore({
+    db: db,
+})
 
 // Templates
 app.use(layouts)
@@ -14,6 +25,21 @@ app.set('view engine', 'ejs')
 
 // Static files
 app.use(express.static(__dirname + '/public'))
+
+// Sessions
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        },
+    })
+)
+sessionStore.sync()
+app.use(passport.authenticate('session'))
 
 // Routes
 app.use('/', require('./config/routes'))
