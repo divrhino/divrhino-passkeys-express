f88d087bd95ef29231a33897032a084dc3eed174
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 22:14:32 +0800

    Import passport-service into routes file

---
+++ app/services/passport-service.js
@@ -0,0 +1,9 @@
+class PassportService {
+    init(store) {
+        // 1. configure passport to use WebAuthn Strategy
+        // 2. passport serialise user
+        // 3. passport deserialise user
+    }
+}
+
+module.exports = PassportService
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+// Passport
+const PassportService = require('../app/services/passport-service')
+const SessionChallengeStore =
+    require('passport-fido2-webauthn').SessionChallengeStore
+
+const passportService = new PassportService()
+const store = new SessionChallengeStore()
+
+passportService.init(store)
+
 // Controllers
 const pages = new (require('../app/controllers/pages'))
 const auth  = new (require('../app/controllers/auth'))
