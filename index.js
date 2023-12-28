9df5e3ece049ba3f6d7328914434b9edab96f1aa
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 21:27:45 +0800

    Add formatting

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 3000
 const host = '0.0.0.0'
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, host, () => {
-  console.log(`Example app listening on http://${host}:${port}`)
+    console.log(`Example app listening on http://${host}:${port}`)
 })
