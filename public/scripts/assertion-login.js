d1411d59cf7bedfdfef9dbf2d3314f92f2f62b72
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 22:57:42 +0800

    Phase 2 - step 1- checkConditionalMediationSupport

--- public/scripts/assertion-login.js
+++ public/scripts/assertion-login.js
@@ -1,12 +1,18 @@
 class Login {
     async init() {
         // 1. Check that conditional mediation is supported
-
+        await this.checkConditionalMediationSupport()
         // 2. Get challenge from server (Relying Party)
         // 3. Use existing public key credential to authenticate user
         // 4. Use public key credential to login user
 
         // 5. Redirect to user's dashboard
+    }
+
+    async checkConditionalMediationSupport() {
+        const isCMA =
+            await window.PublicKeyCredential.isConditionalMediationAvailable()
+        if (!isCMA) return
     }
 }
 
