ae3f76df805be61a3807ed72c0ef6753cbd73210
divrhino <fancydresspenguin+divrhino@gmail.com>
2023-12-28 21:51:00 +0800

    Move `welcome` action into PagesController

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -1,3 +1,8 @@
-class PagesController {}
+class PagesController {
+    welcome(req, res, next) {
+        if (!req.user) return res.send('Hello Universe!')
+        next()
+    }
+}
 
 module.exports = PagesController
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello Universe!')
-})
+// Controllers
+const pages = new (require('../app/controllers/pages'))
+
+router.get('/', pages.welcome)
 
 module.exports = router
